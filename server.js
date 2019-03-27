d6805a816d169644cce912eaf872516e412b029c
VincentKing777 <wrz77888597918@gmail.com>
2019-03-27 15:18:28 -0700

    V

--- server.js
+++ server.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
 var app = express();
+const port = process.env.PORT || 8080;
 
-hbs.registerPartials(__dirname + '/views/partials');
 
-app.set('view engine', 'hbs');
 
-hbs.registerHelper('getCurrentYear', () => {
-    return new Date().getFullYear();
+app.listen(port,() =>{
+    console.log(`Server is up on the port ${port}`);
 });
 
-hbs.registerHelper('message', (text) => {
-    return text.toUpperCase();
-});
 
+
+// const express = require('express');
+// const hbs = require('hbs');
+// const fs = require('fs');
+//
+// var app = express();
+//
+// hbs.registerPartials(__dirname + '/views/partials');
+//
+// app.set('view engine', 'hbs');
+// // app.use(express.static(dirname + '/public'));
+//
+// hbs.registerHelper('getCurrentYear', () => {
+//     return new Date().getFullYear();
+// });
+//
+// hbs.registerHelper('message', (text) => {
+//     return text.toUpperCase();
+// });
+//
 // app.use((request, response, next) => {
 //     // var time = new Date().toString();
 //     // // console.log(${time}: ${request.method} ${request.url});
@@ -26,35 +41,35 @@ hbs.registerHelper('message', (text) => {
 //     //     }
 //     // });
 //     // next();
-//     response.render('maintenance.hbs')
+//     response.render('a.hbs')
 // });
-
-app.get('/', (request, response) => {
-    // response.send('<h1>Hello Express!</h1>');
-    response.send({
-        name: 'Your Name',
-        school: [
-            'BCIT',
-            'SFU',
-            'UBC'
-        ]
-    })
-});
-
-app.get('/info', (request, response) => {
-    response.render('about.hbs', {
-        title: 'About page',
-        // year: new Date().getFullYear(),
-        welcome: 'Hello!'
-    })
-});
-
-app.get('/404', (request, response) => {
-    response.send({
-        error: 'Page not found'
-    })
-});
-
-app.listen(8080, () => {
-    console.log('Server is up on the port 8080');
-});
+//
+// app.get('/', (request, response) => {
+//     // response.send('<h1>Hello Express!</h1>');
+//     response.send({
+//         name: 'Your Name',
+//         school: [
+//             'BCIT',
+//             'SFU',
+//             'UBC'
+//         ]
+//     })
+// });
+//
+// app.get('/info', (request, response) => {
+//     response.render('about.hbs', {
+//         title: 'About page',
+//         // year: new Date().getFullYear(),
+//         welcome: 'Hello!'
+//     })
+// });
+//
+// app.get('/404', (request, response) => {
+//     response.send({
+//         error: 'Page not found'
+//     })
+// });
+//
+// app.listen(8080, () => {
+//     console.log('Server is up on the port 8080');
+// });
